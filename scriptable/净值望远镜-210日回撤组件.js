aa1e1e5012da9915af7786ff5c5f58119045cc46
Geocribug <141498760+Geocribug@users.noreply.github.com>
2026-07-19 23:05:23 +0800

    fix: include 22:00 NAV publication refresh hour

--- scriptable/净值望远镜-210日回撤组件.js
+++ scriptable/净值望远镜-210日回撤组件.js
@@ -14,7 +14,7 @@
 
   function refreshHours() {
     const hour = new Date().getHours();
-    return hour >= 16 && hour < 22 ? 1 : OFF_HOURS_REFRESH_HOURS;
+    return hour >= 16 && hour <= 22 ? 1 : OFF_HOURS_REFRESH_HOURS;
   }
 
   const activeRefreshHours = refreshHours();
